8ce0b5225a0cd19a9338030fd0c5d3cc8b901cb8
OlegGdalevich <gdalevich.oleg@gmail.com>
2024-11-23 13:46:44 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,28 @@
 #ТУТ ИНТЕРФЕЙС НАШЕГО АДМИНА ДЛЯ ПРОСМОТРА ЗА СОТРУДНИКАМИ#
+
+import requests
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.uic import loadUi
+
+from callibri_controller import callibri_controller, ConnectionState, CallibriInfo
+
+
+class MainScreen(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        loadUi("ui/AdminWindow.ui", self)
+
+        self.resetBtn.clicked.connect(self.refresh)
+        
+    def refresh(self):
+      pass
+
+
+app = QApplication(sys.argv)
+window = MainScreen()
+window.show()
+app.exec()
+callibri_controller.stop_all()
+sys.exit()
